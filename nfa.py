8f33c6f83b5c0b5138ef8ac557f674ba7393bd45
gmrperdigao <36706143+gmrperdigao@users.noreply.github.com>
2018-09-09 04:20:23 -0300

    Update nfa.py

--- nfa.py
+++ nfa.py
@@ -1,11 +1,67 @@
-regex = input("Digite expressao regular:")
-print(regex)
-count = 0
-L = []
-abre_par = []
-fecha_par = []
-for i in regex:
-    L.append(i)
+from graphviz import Digraph
+
+class Automata():
+    def __init__(self,expr):
+        expression = ''.join(expr)
+        self.start = 0
+        self.final = 1
+        self.new = 2
+        self.nfa = {0:{expression:1}}
+    
+    def union(self,r1,r2):
+        trans1 = ''.join(r1)
+        trans2 = ''.join(r2)
+        orig = trans1+'+'+trans2
+        orig2 = '('+orig+')'
+        for origem, c in self.nfa.items():
+            for transicao,destino in self.nfa[origem].items():
+                if transicao == orig or transicao == orig2:
+                    aux_o = origem
+                    aux_d = destino
+                    aux_t = transicao
+        del self.nfa[aux_o][aux_t]
+        self.nfa[aux_o][trans1] = aux_d
+        self.nfa[aux_o][trans2] = aux_d
+                    
+    
+    def concat(self,r1,r2):
+        trans1 = ''.join(r1)
+        trans2 = ''.join(r2)
+        if '+' in trans2:
+            trans2 = '('+trans2+')'
+        orig = trans1+trans2
+        for origem,c  in self.nfa.items():
+            for transicao,destino in self.nfa[origem].items():
+                if transicao == orig:
+                    aux_o = origem
+                    aux_d = destino
+                    aux_t = transicao
+        del self.nfa[aux_o][aux_t]
+        self.nfa[aux_o][trans1] = self.new
+        if self.new not in self.nfa.keys():
+            self.nfa[self.new] = {trans2:aux_d}
+        else: self.nfa[self.new][trans2] = aux_d
+        self.new = self.new+1
+    
+    def kleene(self,r1):
+        trans = ''.join(r1)
+        alt = '('+trans+')*'
+        for origem,c  in self.nfa.items():
+            for transicao,destino in self.nfa[origem].items():
+                if transicao == trans or transicao == alt:
+                    aux_o = origem
+                    aux_d = destino
+                    aux_t = transicao
+        n = len(aux_t)
+        del self.nfa[aux_o][aux_t]
+        if n == 2:
+            aux_t = aux_t[0:1] 
+        self.nfa[aux_o]['&'] = self.new
+        if self.new not in self.nfa.keys():
+            self.nfa[self.new] = {'&':aux_d}
+        else: self.nfa[self.new]['&'] = aux_d
+        self.nfa[self.new][aux_t] = self.new
+        self.new = self.new+1
 
 
 def recursao(expr):
@@ -15,6 +71,8 @@ def recursao(expr):
     testa = 1
     n = len(expr)
     if n <= 2:
+        if n == 2:
+            afn.kleene(expr)
         return 0
     count = 0
     for i in range(0,n-1):
@@ -26,6 +84,7 @@ def recursao(expr):
             r1 = expr[0:i]
             r2 = expr[i+1:n]
             testa = 0
+            afn.union(r1,r2)
             recursao(r1)
             recursao(r2)
     count = 0
@@ -36,6 +95,7 @@ def recursao(expr):
         #fecho de kleene
         r1 = expr[1:n-2]
         testa = 0
+        afn.kleene(r1)
         recursao(r1)
     count = 0
     for i in range(0,n-1):
@@ -44,6 +104,7 @@ def recursao(expr):
                 if (previous == '*' or previous.isalpha()) and count == 0:
                     r1 = expr[0:i]
                     r2 = expr[i:n]
+                    afn.concat(r1,r2)
                     recursao(r1)
                     recursao(r2)
                     testa = 0
@@ -54,6 +115,7 @@ def recursao(expr):
                         r2 = expr[i:n]
                     elif expr[n-1] == ')':
                         r2 = expr[i+1:n-1]
+                    afn.concat(r1,r2)
                     recursao(r1)
                     recursao(r2)
                     testa = 0
@@ -64,10 +126,14 @@ def recursao(expr):
                     if expr[i+1] == '*':
                         """ fecho de kleene"""
                         r1 = expr[0:i+2]
-                        recursao(r1)
                         if i != n-2:
                             r2 = expr[i+2:n]
+                        if i != n-2:
+                            afn.concat(r1,r2)
+                            recursao(r1)
                             recursao(r2)
+                        else:
+                            recursao(r1)
                         testa = 0
                     else:
                         r1 = expr[1:i]
@@ -77,12 +143,20 @@ def recursao(expr):
                 if count == 0:
                     r1 = expr[0:i+1]
                     r2 = expr[i+1:n]
+                    print(r1,'----',r2)
+                    afn.concat(r1,r2)
+                    print(afn.nfa)
                     recursao(r1)
                     recursao(r2)
                     testa = 0
             previous = expr[i]
                 
-            
-recursao(L)
-regex = input("Digite expressao regular:")     
-            
+regex = input("Digite expressao regular:")
+count = 0
+L = []
+for i in regex:
+    L.append(i)
+
+afn = Automata(L)
+recursao(L)    
+print(afn.nfa)
